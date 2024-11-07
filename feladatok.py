4444dcdb655cd1cd2284faba546fb77fb185731d
S4mur419020 <muraitomi04@gmail.com>
2024-11-07 08:58:16 +0100

    2.feladat

--- feladatok.py
+++ feladatok.py
@@ -9,4 +9,24 @@ def feladat1():
     print(szam)
     
 def feladat2():
+    lista=[]
+    i=0
+    while i<13:
+        rszam:int=int(random.random()*(141)+10)
+        lista.append(rszam)
+        i+=1
+    return lista
+
+def feladat2_2(lista):
+    i:int=0
+    db:int=0
+    while(i<len(lista)):
+        if(lista[i]%3==0):
+            db+=1
+        i+=1
+        
+    return db
+
+def feladat3(szov, szam):
+    
     
--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import feladatok
 
 print("1. feladat")
 feladatok.feladat1()
+
+print("2.feladat")
+lista=feladatok.feladat2()
+print(lista)
+db=feladatok.feladat2_2(lista)
+print(f"A számok között {db} db 3-mal osztható van!")
